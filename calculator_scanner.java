fa27960ea3faa3d4752d6405dd1b65ed52bffc28
SAC_CS112_Do_Xuan <xdo0410@gmail.com>
2014-09-15 22:08:47 -0700

    Create calculator_scanner.java

---
+++ calculator_scanner.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+
+
+public class calculator_scanner {
+
+
+    public static void main(String[] args) {
+
+        Scanner calculator = new Scanner(System.in);
+
+        System.out.println("Simple Calculator");
+
+        System.out.println("\nHere are your options:");
+        System.out.println("\n1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Division");
+        System.out.println("4. Multiplication");
+
+        System.out.print("\nWhat would you like to do?: ");
+        int choice = calculator.nextInt();
+        System.out.println();
+
+
+        if (choice == 1){
+            addition();
+        }
+        else if (choice == 2){
+            subtraction();
+        }
+        else if (choice == 3){
+            division();
+        }
+        else if (choice == 4){
+            multiplication();
+        }
+
+        System.out.println();
+        calculator.close();
+    }
+
+    public static void addition(){
+
+        int nOne, nTwo;
+        Scanner calculator = new Scanner(System.in);
+
+        System.out.println("Addition");
+
+        System.out.print("\nFirst Number: ");
+        nOne = calculator.nextInt();
+
+        System.out.print("\nSecond Number: ");
+        nTwo = calculator.nextInt();
+
+        calculator.close();
+        System.out.println("\nSum: " + nOne + " + " + nTwo + " = " + (nOne + nTwo));
+    }
+
+    public static void subtraction(){
+        int nOne, nTwo;
+        Scanner calculator = new Scanner(System.in);
+
+        System.out.println("Subtraction");
+
+        System.out.print("\nFirst Number: ");
+        nOne = calculator.nextInt();
+
+        System.out.print("\nSecond Number: ");
+        nTwo = calculator.nextInt();
+
+        calculator.close();
+        System.out.println("\nSum: " + nOne + " - " + nTwo + " = " + (nOne - nTwo));
+    }
+
+    public static void division(){
+        int nOne, nTwo;
+        Scanner calculator = new Scanner(System.in);
+
+        System.out.println("Division");
+
+        System.out.print("\nFirst Number: ");
+        nOne = calculator.nextInt();
+
+        System.out.print("\nSecond Number: ");
+        nTwo = calculator.nextInt();
+
+        calculator.close();
+        System.out.println("\nSum: " + nOne + " / " + nTwo + " = " + (nOne / nTwo));
+    }
+
+    public static void multiplication(){
+        int nOne, nTwo;
+        Scanner calculator = new Scanner(System.in);
+
+        System.out.println("Multiplication");
+
+        System.out.print("\nFirst Number: ");
+        nOne = calculator.nextInt();
+
+        System.out.print("\nSecond Number: ");
+        nTwo = calculator.nextInt();
+
+        calculator.close();
+        System.out.println("\nSum: " + nOne + " x " + nTwo + " = " + (nOne * nTwo));
+    }
+}
